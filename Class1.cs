a341778937e2b76a35ecfa5c40a6bb995a4945b9
Khalaf Nasirov <khalafnasirs@gmail.com>
2023-03-30 13:47:04 +0400

    Added with git

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿namespace Helper
+{
+	public static class Cons
+	{
+		public static void Print()
+		{
+			Console.WriteLine();
+		}
+
+		public static void Print(object obj)
+		{
+			string? msg = obj.ToString();
+
+			Console.WriteLine(msg);
+		}
+
+		public static void SeperateLine(int length)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				Console.Write("-");
+			}
+			Console.WriteLine();
+		}
+	}
+
+	public static class StringOps
+	{
+		public static string Combine(object ob1,  object ob2)
+		{
+			return ob1?.ToString() + ob2?.ToString();
+		}
+
+        public static string Combine(object ob1, object ob2, object ob3)
+        {
+			return ob1?.ToString() + ob2?.ToString() + ob3?.ToString();
+        }
+
+        public static string Combine(object ob1, object ob2, object ob3, object ob4)
+        {
+            return ob1?.ToString() + ob2?.ToString() + ob3?.ToString() + ob4?.ToString();
+        }
+
+        public static string Combine(object ob1, object ob2, object ob3, object ob4, object ob5)
+        {
+			return ob1?.ToString() + ob2?.ToString() + ob3?.ToString() + ob4?.ToString() + ob5?.ToString();
+        }
+
+		public static string Combine(params object[] args)
+		{
+			string? full = string.Empty;
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				full += args[i].ToString();
+			}
+
+			return full;
+		}
+    }
+}
